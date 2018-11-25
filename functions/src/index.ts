e439b86f5aabd4654ec82717aacf4564203432da
t-Asai <takumi.a42@gmail.com>
2018-11-25 15:47:47 +0900

    actions on googleと連携

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,21 @@
-import * as functions from 'firebase-functions';
+"use strict";
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+// Import the Dialogflow module from the Actions on Google client library.
+const { dialogflow } = require("actions-on-google");
 
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+// Import the firebase-functions package for deployment.
+const functions = require("firebase-functions");
+
+// Instantiate the Dialogflow client.
+const app = dialogflow({ debug: true });
+
+// Handle the Dialogflow intent named 'favorite color'.
+// The intent collects a parameter named 'color'.
+app.intent("favorite color", (conv, { color }) => {
+  const luckyNumber = color.length;
+  // Respond with the user's lucky number and end the conversation.
+  conv.close(`Your lucky number is ${luckyNumber}`);
 });
+
+// Set the DialogflowApp object to handle the HTTPS POST request.
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
